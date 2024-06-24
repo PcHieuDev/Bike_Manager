7c0b7729ae6a81338768449c30cd45bb4731c732
HieuPhan <phanhieu164nt@gmail.com>
2024-06-24 14:00:16 +0700

    first commit

--- app/Exceptions/ErrorRegisteringUserException.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Exceptions;
-
-use Exception;
-
-class ErrorRegisteringUserException extends Exception
-{
-    //
-}
--- app/Exceptions/ProductNotFoundException.php
+++
@@ -1,9 +0,0 @@
-<?php 
-namespace App\Exceptions;
-
-use Exception;
-
-class ProductNotFoundException extends Exception
-{
-    //
-}
